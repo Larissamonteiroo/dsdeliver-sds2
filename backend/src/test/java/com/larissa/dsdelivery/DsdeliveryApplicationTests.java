27bf67a4b6d9dc618d2c590e825cbddc431b771b
Larissa Monteiro <larissa.marques.1999@hotmail.com>
2021-09-17 01:04:31 -0300

    Created project

---
+++ backend/src/test/java/com/larissa/dsdelivery/DsdeliveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.larissa.dsdelivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsdeliveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
